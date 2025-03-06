13992de8c9d3b3ece3f401144fdb82aabbf4e0cf
정성빈 <tjdqls3124@gmail.com>
2025-03-06 13:30:42 +0900

    파일 이름 변경

---
+++ src/src/jdbc/DBManager.java
@@ -0,0 +1,44 @@
+package jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+// Connection 객체를 생성, 전달
+// ResultSet, PreparedStatement, Connection 객체 종료 close()
+public class DBManager {
+    static String url = "jdbc:mysql://localhost:3306/madangdb";
+    static String user = "root";
+    static String pwd = "root";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, pwd);
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+    // releaseConnection Variable Args... (개인적으로는 별로 for 때문)
+    public static void releaseConnection(PreparedStatement pstmt, Connection con) {
+        try {
+            pstmt.close();
+            con.close();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void releaseConnection(ResultSet rs, PreparedStatement pstmt, Connection con) {
+        try {
+            if ( rs != null) rs.close();
+            if ( pstmt != null) pstmt.close();
+            if ( con != null) con.close();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
